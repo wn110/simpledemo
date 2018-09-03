b342a1b19caa5d1bd0bb56d2cb4a55a1796d549b
wn110 <m17600580245@163.com>
2018-09-03 15:34:47 +0800

    修改

--- src/main/java/com/pcitc/controller/OrderController.java
+++ src/main/java/com/pcitc/controller/OrderController.java
@@ -1,26 +1,14 @@
 package com.pcitc.controller;
 
-import com.pcitc.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.pcitc.model.Order;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class OrderController {
-
-    @Autowired
-    OrderService orderService;
-
-    @RequestMapping("queryOrder")
-    public String selectOrderByOrderid(Long orderid, HttpServletRequest request){
-        Order order = orderService.selectByPrimaryKey(orderid);
-        request.setAttribute("order", order);
-        return "showOrder" ;
-    }
 
     @RequestMapping("universe")
     @ResponseBody
--- src/main/java/com/pcitc/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.pcitc.service;
-
-import com.pcitc.model.Order;
-
-public interface OrderService {
-
-    Order selectByPrimaryKey(Long orderid);
-
-}
